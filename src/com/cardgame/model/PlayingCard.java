266a77c4a6f91143327be1af9ad5833252cd630c
DSARRAZY <d.sarrazy@gmail.com>
2021-05-03 08:59:53 +0200

    playingcard

--- src/com/cardgame/model/PlayingCard.java
+++ src/com/cardgame/model/PlayingCard.java
@@ -1,5 +1,30 @@
 package com.cardgame.model;
 
 public class PlayingCard {
-
+	private Rank rank;
+	private Suit suit;
+	private boolean faceup;
+	
+	public PlayingCard(Rank rank, Suit suit ) {
+		super();
+		this.rank = rank;
+		this.suit = suit;
+	}
+	
+	public Rank getRank() {
+		return rank;
+	}
+	
+	public Suit getSuit() {
+		return suit;
+	}
+	
+	public boolean isFaceUp() {
+		return faceUp;
+	}
+	
+	public boolean flip() {
+		faceUp = !faceUp;
+		return faceUp;
+	}
 }
